aad53b06c4240fc34f7272521cf38643468ef731
0rogerinho <luaansilva11@gmail.com>
2023-10-23 17:48:45 -0300

    feat: add theme light and provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>pagina inicial</p>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Test
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
